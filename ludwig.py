2122e629479729a92d2ec2d3605c6e7cb347f741
Khazbs <khazbartur@yandex.ru>
2018-12-07 12:26:04 +0300

    Add basic terms of Wittgenstein philosophy

---
+++ ludwig.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+def logic_projection(entity, correct=True):  # TODO: class
+	'''Данная функкция симулирует проецирование неких сущностей на логическое пространство'''
+	entity_projection = entity
+	return entity_projection
+
+def union(iterable, start=set()):
+	for item in iterable:
+		start |= item
+	return start
+
+class World:
+	'''Мир'''
+	facts = set()  # Целокупность фактов (1.1)
+	substance = objects = set()  # Объекты образуют субстанцию мира (2.021)
+	def __init__(self, facts):
+		self.facts |= set(facts)
+		self.substance |= union(union(set(event.structure.keys()) for event in fact.events) for fact in facts)
+
+class Fact:
+	'''Факт'''
+	events = set()  # Со-бытия, существование которых характеризует факт
+	def __init__(self, events):
+		self.events |= set(events)
+
+class Event:
+	'''Со-бытие'''
+	structure = relations = dict()  # Структура события, то есть способ соотношения объектов в со-бытии (2.032, 2.031)
+	def __init__(self, relations):
+		for relation in relations:
+			relation.first_object.form |= {self}  # Если объект участвует в событии, то его форма содержит возможность этого события
+			relation.second_object.form |= {self}  # Если объект участвует в событии, то его форма содержит возможность этого события
+			if relation.first_object in self.structure:
+				self.structure[relation.first_object] += relation
+			else:
+				self.structure[relation.first_object] = [relation]
+			if relation.second_object in self.structure:
+				self.structure[relation.second_object] += relation ** -1
+			else:
+				self.structure[relation.second_object] = [relation ** -1]
+
+class Object:
+	'''Объект, предмет, вещь'''
+	form = possible_events = set()  # Возможность объекта вхождения в со-бытия - его форма (2.0141)
+	def __init__(self, possible_events=set()):
+		self.form |= possible_events
+
+class Interaction:
+	'''Способ взаимодействия между предметами'''
+	interaction = None
+	inverse = None  # Является ли способ взаимодействия обратным (то есть взаимодействием от B к A вместо от A к B)
+	def __init__(self, interaction, inverse=False):
+		self.interaction = str(interaction)
+		self.inverse = inverse
+	def __pow__(self, power):  # Оператор для определения обратного взаимодействия
+		if power not in (-1, 1):
+			raise NotImplementedError
+		elif power == -1:
+			return Interaction(self.interaction, not self.inverse)
+	def __repr__(self):
+		return ("inverse of " if self.inverse else "") + self.interaction
+
+class Relation:
+	'''Соотношение между объектами'''
+	first_object = None
+	second_object = None
+	interaction = None  # Способ соотношения между объектами
+	def __init__(self, first_object, second_object, interaction):
+		self.first_object = first_object
+		self.second_object = second_object
+		self.interaction = interaction
+	def __pow__(self, power):  # Оператор для определения обратного соотношения
+		if power not in (-1, 1):
+			raise NotImplementedError
+		elif power == -1:
+			return Relation(self.second_object, self.first_object, self.interaction ** -1)
+
+class Picture:
+	'''Картина'''
+	elements = None  # Объектам в артине соответствуют элементы картины
+	meaning = None  # Смысл картины - то, что она изображает
+	def __init(self, world):
+		self.meaning = world  # Картина - модель действительности
+		self.elements = set(map(logic_projection, world.substance))  # ...в логическом пространстве
+
+class Thought:  # Мысль
+	pass
+
+class Symbol:  # Знак
+	pass
+
+class Sentence:  # Предложение
+	pass
+
+class Symtence:  # Знак-предложение
+	pass
+
+print('--- Ludwig module has been loaded ---')
